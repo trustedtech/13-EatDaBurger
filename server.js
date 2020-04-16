00cfbd0f079a3771d9edc1e0aae47fdc604b1da7
trustedtech <brian@trusted.tech>
2020-04-16 16:53:03 -0400

    Setup essential file/folder structure;  NPM required modules;  Begin coding of essential HTML

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+// const routes = require("./controllers/burgers_controller.js");
+require("./routes/routesAPI")(app);
+require("./routes/routesHTML")(app);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
+app.use(routes);
 
-require("./routes/routesAPI")(app);
-require("./routes/routesHTML")(app);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.listen(PORT, function() {
   console.log("App is now listening on PORT: " + PORT);
