160c4b609ca19a43ef039477d31dc47dafeb87dd
trustedtech <brian@trusted.tech>
2020-04-23 23:09:12 -0400

    Prep for Heroku deploy.

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql2');
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "dno6xji1n8fm828n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
+    user: "rb3xr6fgvh4uqxmr",
+    password: "ng9ph002h93c0lr7",
+    database: "azr3mpsnoph3txnf"
 });
 
 connection.connect();
